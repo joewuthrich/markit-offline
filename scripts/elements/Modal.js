6e29599af9435cf79389f5f294f9073eae838bca
Joe Wuthrich <joerwuthrich@gmail.com>
2021-11-04 14:47:57 +1300

    Added close button to modal

--- scripts/elements/Modal.js
+++ scripts/elements/Modal.js
@@ -8,12 +8,23 @@ export default class Modal extends HTMLElement {
     this.id = "tmw-modal-container";
     this.innerHTML =
       `
-    <div class="tmw-modal-container-inner">` +
+    <div class="tmw-modal-container-inner">
+      <svg data-layer="88c460d8-8934-4c4d-9894-7ba998c3a406" preserveAspectRatio="none" 
+      viewBox="5 5 20 20" class="tmw-close-modal-icon tmw-icon">
+          <path d="M 25 7.014285087585449 L 22.9857120513916 5 L 15 12.98571491241455 L 7.014285087585449 
+          5 L 5 7.014285087585449 L 12.98571491241455 15 L 5 22.9857120513916 L 7.014285087585449 25 L 15 
+          17.01428413391113 L 22.9857120513916 25 L 25 22.9857120513916 L 17.01428413391113 15 L 25 
+          7.014285087585449 Z"></path>
+      </svg>` +
       content +
       `</div>`;
 
     this.addEventListener("click", function (event) {
       if (event.target == this) this.remove();
+    });
+
+    this.children[0].children[0].addEventListener("click", () => {
+      this.remove();
     });
 
     document.getElementById("tmw-body").append(this);

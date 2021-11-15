43732dd127a4c26babb0dc5324e412932046b03f
Joe Wuthrich <joerwuthrich@gmail.com>
2021-11-15 20:59:04 +1300

    Fixed issue with count increasing when editing

--- scripts/elements/Comment.js
+++ scripts/elements/Comment.js
@@ -19,9 +19,12 @@ export default class Comment extends HTMLElement {
         event.target == icons[2] ||
         event.target == icons[3] ||
         event.target == icons[4] ||
-        event.target == icons[2].children[0]
+        event.target == icons[2].children[0] ||
+        event.target == icons[3].children[0] ||
+        event.target == icons[4].children[0]
       )
         return;
+      console.log(event.target);
       navigator.clipboard.writeText(this.children[0].textContent).then(
         () => {
           this.count++;
